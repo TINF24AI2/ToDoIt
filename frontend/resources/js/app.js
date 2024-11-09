94167f96d0b8ebb2a4aa32c07dbd899f67cdb270
Nik <127861651+NikMitzel@users.noreply.github.com>
2024-11-09 10:47:45 +0100

    Menu first logic, res. , first steps in todo

--- frontend/resources/js/app.js
+++ frontend/resources/js/app.js
@@ -1,6 +1,14 @@
+function onLoad() {
+    //checkSession();
+    setUsername();
+    loadThemePreference();
+    loadLists();
+}
+
+
 //Check if the user still logged in
 function checkSession() {
-    if (getCookie("session") = null) {
+    if (getCookie("session") == null) { // Change '=' to '=='
        window.location.href = 'index.html';
    }
 }
@@ -17,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
 /* Function */
 function performSearch(){
    const searchInput = document.querySelector(".header_search");
-   //Task1 - Search for ToDos
+   alert("Search is a work in progress. You searched for: " + searchInput.value);
 }
 
 function handleKeyPress(event) {
@@ -41,15 +49,28 @@ function setUsername(){
     }
 }
 
-function openPopup(id){
+function openPopup(id) {
     const popup = document.getElementById(id);
     if (popup.style.display === 'flex' || popup.style.display === '') {
         popup.style.display = 'none';
+        document.body.classList.remove('popup-open');
     } else {
         popup.style.display = 'flex';
+        document.body.classList.add('popup-open');
     }
 }
 
-function closePopup(id){
+function closePopup(id) {
     document.getElementById(id).style.display = 'none';
+    document.body.classList.remove('popup-open');
+}
+
+function printList(id, name) { 
+    document.getElementById('menu_list_menu').innerHTML += `
+    <div id="${id}" class="menu_list">
+        <svg width="2rem" height="2rem" viewBox="-1 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg">
+            <path d="M16.417 9.583A7.917 7.917 0 1 1 8.5 1.666a7.917 7.917 0 0 1 7.917 7.917zM5.164 6.17a.792.792 0 0 0-.792-.792H4.37a.792.792 0 0 0 0 1.583h.002a.792.792 0 0 0 .792-.791zm0 3.475a.792.792 0 0 0-.792-.791H4.37a.792.792 0 0 0 0 1.583h.002a.792.792 0 0 0 .792-.792zm0 3.476a.792.792 0 0 0-.792-.791H4.37a.792.792 0 1 0 0 1.583h.002a.792.792 0 0 0 .792-.792zM13.45 6.17a.792.792 0 0 0-.792-.791H6.922a.792.792 0 1 0 0 1.583h5.736a.792.792 0 0 0 .792-.791zm0 3.476a.792.792 0 0 0-.792-.791H6.922a.792.792 0 1 0 0 1.583h5.736a.792.792 0 0 0 .792-.792zm0 3.476a.792.792 0 0 0-.792-.791H6.922a.792.792 0 1 0 0 1.583h5.736a.792.792 0 0 0 .792-.792z"/>
+        </svg>
+        <h3>${name}</h3>
+    </div>`;
 }
--- frontend/resources/js/backendconnection.js
+++ frontend/resources/js/backendconnection.js
@@ -1,5 +1,6 @@
 // This function will be used to login the user return 0 on success and 1 on rong password and 2 on wrong username
 function loginBack(username, password) {
+    //Task 1: Make BackendConection to the backend to login the user and set the secion cookie and username cookie
     console.log("loginBack called with username:", username, "and password:", password);
     // Simulate backend connection
     if (username && password) {
@@ -19,4 +20,10 @@ function loginBack(username, password) {
 function signupBack(username, password) {
     //Task 2: Make BackendConection to the backend to register the user and refers to login function to login the user
     return -1;
+}
+
+
+function loadLists() {
+    //Task 3: Make BackendConection to the backend to get the lists refer to printList to print the lists
+    printList(1, "List 1");
 }
